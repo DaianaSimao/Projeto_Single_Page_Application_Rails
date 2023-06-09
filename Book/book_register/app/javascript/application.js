21846abde94c14e2effd0568885726237b581bc1
DaianaSimao <daiana.xd11@hotmail.com>
2023-06-08 22:21:52 -0300

    Estilização da index + Estilização do formulário

--- Book/book_register/app/javascript/application.js
+++ Book/book_register/app/javascript/application.js
@@ -2,4 +2,42 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+function startTimer(duration, display) {
+
+  var timer = duration, minutes, seconds;
+
+  setInterval(function () {
+
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = 0;
+          document.querySelector("#vinculo").disabled = true;
+          document.querySelector("#autor").disabled = true;
+          document.querySelector("#issn").disabled = true;
+          document.querySelector("#editora").disabled = true;
+          document.querySelector("#titulo").disabled = true;
+          document.querySelector("#edicao").disabled = true;
+          document.querySelector("#ano").disabled = true;
+          document.querySelector("#button").disabled = true;
+          display.textContent = 'Acabou';
+          console.log('ok');
+      }
+
+  }, 1000);
+}
+window.onload = function () {
+
+  var duration = (12 * 10 ) - 1;
+  var display = document.querySelector("#timer"); 
+
+  startTimer(duration, display);
+}
  
